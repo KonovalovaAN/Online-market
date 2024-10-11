331792a66a61e886ae4d6d142d9d4e3fdad6d2b8
TemaBlag <114094206+TemaBlag@users.noreply.github.com>
2024-10-11 20:08:28 +0300

    Delete pressF/Architecture/urls.py

--- pressF/Architecture/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import UserView
-
-urlpatterns = [
-    path('users/', UserView.as_view(), name='user-list'),            # Для работы с коллекцией пользователей
-    path('users/<int:id>/', UserView.as_view(), name='user-detail'),  # Для работы с конкретным пользователем
-]
