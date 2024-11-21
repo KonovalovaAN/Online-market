8443445859b0022e4f6286b6112992ac56a5dbeb
Levaaaaaa <levasneshko@tut.by>
2024-11-21 12:03:56 +0300

    add SQLite db

--- pressF/goods/models.py
+++ pressF/goods/models.py
@@ -7,3 +7,24 @@ class Categories(models.Model):
 
     class Meta:
         db_table = 'category'
+    
+    def __str__(self):
+        return self.name
+    
+
+class Products(models.Model):
+    name = models.CharField(max_length=150, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True)
+    description = models.TextField(blank=True, null=True, verbose_name='Описание')
+    image = models.ImageField(upload_to='goods_images', blank=True, null=True, verbose_name='Изображение')
+    price = models.DecimalField(default=0.00, max_digits=7, decimal_places=2, verbose_name='Цены')
+    discount = models.DecimalField(default=0.00, max_digits=7, decimal_places=2, verbose_name='Скидка в %')
+    quantity = models.PositiveIntegerField(default=0, verbose_name='Количество')
+    category = models.ForeignKey(to=Categories(), on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'product'
+
+    def __str__(self):
+        return f'{self.name} Количество - {self.quantity}'
+    
--- pressF/goods/views.py
+++ pressF/goods/views.py
@@ -6,6 +6,8 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 
+from goods.models import Products
+
 class ProductListView(APIView):
     permission_classes = [IsAuthenticated]
 
@@ -18,15 +20,17 @@ class ProductListView(APIView):
 #Swagger
 
 def catalog(request):
-    goods = [{
-        'image' : 'deps/images/goods/tablet3.jpg',
+    '''goods = [{        'image' : 'deps/images/goods/tablet3.jpg',
         'name' : 'Планшет Teclast P30T 4GB/128GB (серый)',
         'description' : 'some description',
         'price' : 1.00
-    }]
+    }]'''
+    images = ['deps/images/goods/tablet3.jpg']
+    goods = zip(Products.objects.all(), images)
+
     context = {
         'title' : 'TechKing - Каталог',
-        'goods' : goods
+        'goods' : goods,
     }
     return render(request, 'goods/catalog.html', context)
 
