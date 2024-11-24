cd83dc2db72652118d470e9690c414288bbf3506
Levaaaaaa <levasneshko@tut.by>
2024-11-24 20:52:37 +0300

    add SQLite db

--- pressF/pressF/urls.py
+++ pressF/pressF/urls.py
@@ -19,6 +19,9 @@ from django.contrib import admin
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.conf.urls.static import static
+
+from pressF import settings
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -42,3 +45,11 @@ urlpatterns = [
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('swagger.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
 ]
+
+if settings.DEBUG:
+#    urlpatterns += [
+#        path('__debug__/', include('debug_toolbar.urls'))
+#    ]
+    
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
